d1f77b966299fb7b963592be485f2f102b6d2979
June Kimm <120693222+rlawns255@users.noreply.github.com>
2023-03-02 23:35:39 -0600

    Add files via upload

--- Kimm_quaries.sql
+++ Kimm_quaries.sql
@@ -7,9 +7,13 @@ FROM employees
 INNER JOIN salaries ON
 employees.emp_no = salaries.emp_no;
 
-SELECT employees.last_name,
-	employees.first_name,
-	employees.hire_date,
-FROM employees
-WHERE 
+SELECT last_name,
+	first_name,
+	hire_date
+FROM employees E
+WHERE E.hire_date > 1985/12/31 AND E.hire_date < 1987/1/1;
+
+
+
+
 	
