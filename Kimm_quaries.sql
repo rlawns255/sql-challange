2605199234c7e49f3cc8de3b3109bfbee9011e51
June Kimm <120693222+rlawns255@users.noreply.github.com>
2023-03-02 23:57:25 -0600

    Add files via upload

--- Kimm_quaries.sql
+++ Kimm_quaries.sql
@@ -7,12 +7,73 @@ FROM employees
 INNER JOIN salaries ON
 employees.emp_no = salaries.emp_no;
 
+
+
 SELECT last_name,
 	first_name,
 	hire_date
 FROM employees E
 WHERE E.hire_date > 1985/12/31 AND E.hire_date < 1987/1/1;
 
+
+
+SELECT dept_manager.dept_no,
+	departments.dept_name,
+	dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM employees
+INNER JOIN dept_manager ON
+employees.emp_no = dept_manager.emp_no
+INNER JOIN departments ON
+dept_manager.dept_no = departments.dept_no;
+
+
+
+SELECT departments.dept_no,
+	dept_emp.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+departments.dept_no = dept_emp.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no;
+
+
+SELECT first_name,
+	last_name,
+	sex
+FROM employees
+WHERE first_name = 'Hercules' AND Last_Name LIKE 'B%';
+
+SELECT dept_emp.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+departments.dept_no = dept_emp.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no
+WHERE departments.dept_name = 'Sales';
+
+
+SELECT dept_emp.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+departments.dept_no = dept_emp.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
+
+
+SELECT COUNT (last_name)
+FROM employees;
 
 
 
